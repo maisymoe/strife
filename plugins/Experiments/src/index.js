93c41b58620403125c68b54e227712a414c1f009
Beef <beefers@riseup.net>
2023-01-08 23:28:30 +0000

    [Plugins > Experiments] Revert changes

--- plugins/Experiments/src/index.js
+++ plugins/Experiments/src/index.js
@@ -3,27 +3,11 @@ const { findByProps } = vendetta.metro;
 const userMod = findByProps("getCurrentUser");
 const experimentMod = findByProps("getSerializedState");
 const handlers = userMod._dispatcher._actionHandlers._computeOrderedActionHandlers("OVERLAY_INITIALIZE").filter(h => h.name.includes("Experiment"));
-const origFlags = userMod.getCurrentUser().flags;
 
-function setExperiments(flags, expStore, bitwise = false) {
-    if (bitwise) {
-        userMod.getCurrentUser().flags |= 1;
-    } else {
-        userMod.getCurrentUser().flags = flags;
-    }
-    for (let h of handlers) h.actionHandler({ serializedExperimentStore: expStore, user: { flags: flags } });
-}
-
-setExperiments(1, experimentMod.getSerializedState(), true);
+userMod.getCurrentUser().flags |= 1;
+for (let h of handlers) h.actionHandler({ serializedExperimentStore: experimentMod.getSerializedState(), user: { flags: 1 } });
 
 export function onUnload() {
-    setExperiments(origFlags, {
-        guildExperimentOverrides: {},
-        hasLoadedExperiments: false,
-        loadedGuildExperiments: {},
-        loadedUserExperiments: {},
-        trackedExposureExperiments: {},
-        userExperimentOverrides: {},
-    });
+    // TODO: Is this really necessary?
     alert("For experiments and other developer features to be disabled, you must restart Discord.");
 }
