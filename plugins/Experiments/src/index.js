fbf4ee21077643e74d9d1b2badf769e1d221fe4f
Beef <beefers@riseup.net>
2023-01-08 16:10:00 +0000

    [Plugins > Experiments] Attempt unloading

--- plugins/Experiments/src/index.js
+++ plugins/Experiments/src/index.js
@@ -3,11 +3,27 @@ const { findByProps } = vendetta.metro;
 const userMod = findByProps("getCurrentUser");
 const experimentMod = findByProps("getSerializedState");
 const handlers = userMod._dispatcher._actionHandlers._computeOrderedActionHandlers("OVERLAY_INITIALIZE").filter(h => h.name.includes("Experiment"));
+const origFlags = userMod.getCurrentUser().flags;
 
-userMod.getCurrentUser().flags |= 1;
-for (let h of handlers) h.actionHandler({ serializedExperimentStore: experimentMod.getSerializedState(), user: { flags: 1 } });
+function setExperiments(flags, expStore, bitwise = false) {
+    if (bitwise) {
+        userMod.getCurrentUser().flags |= 1;
+    } else {
+        userMod.getCurrentUser().flags = flags;
+    }
+    for (let h of handlers) h.actionHandler({ serializedExperimentStore: expStore, user: { flags: origFlags } });
+}
+
+setExperiments(1, experimentMod.getSerializedState(), true);
 
 export function onUnload() {
-    // TODO: Is this really necessary?
-    alert("For developer features to be fully disabled, you must restart Discord.");
+    setExperiments(origFlags, {
+        guildExperimentOverrides: {},
+        hasLoadedExperiments: false,
+        loadedGuildExperiments: {},
+        loadedUserExperiments: {},
+        trackedExposureExperiments: {},
+        userExperimentOverrides: {},
+    });
+    alert("For experiments and other developer features to be disabled, you must restart Discord.");
 }
