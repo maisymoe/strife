7a17d914344ec9868bd88285e0edaca4fc42fd01
Beefers <Beefers@users.noreply.github.com>
2023-01-08 16:09:45 +0000

    deploy: fbf4ee21077643e74d9d1b2badf769e1d221fe4f

--- test/index.js
+++
@@ -1,1 +0,0 @@
-(function(n){"use strict";const{FormText:e}=vendetta.ui.components.Forms;var t=()=>React.createElement(React.Fragment,null,React.createElement(e,null,"Hello, world!"));const{logger:o}=vendetta;function l(){o.log("Hello world!")}function a(){o.log("Goodbye, world.")}return n.onLoad=l,n.onUnload=a,n.settings=t,n})({});
