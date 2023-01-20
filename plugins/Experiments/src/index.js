782c4a417d4cc267080a02d3d5fd3d2be218c622
Dziurwa <54191536+Dziurwa14@users.noreply.github.com>
2023-01-20 01:07:43 +0100

    [Plugins > Experiments] Fix Experiments

--- plugins/Experiments/src/index.js
+++ plugins/Experiments/src/index.js
@@ -1,23 +1,19 @@
 const { findByProps } = vendetta.metro;
-const { after } = vendetta.patcher;
 
-const userMod = findByProps("getUsers");
-const experimentMod = findByProps("isDeveloper");
-const nodes = Object.values(experimentMod._dispatcher._actionHandlers._dependencyGraph.nodes);
+const userMod = findByProps("getCurrentUser","_dispatcher");
 
-function setExperiments(state) {
-    let gcUserPatch = after("getCurrentUser", userMod, (args, ret) => ({ ...ret, hasFlag: () => state, isStaff: () => state }));
-    try { nodes.find(x => x.name === "ExperimentStore").actionHandler["CONNECTION_OPEN"]({ user: { flags: state ? 1 : 0 }, type: "CONNECTION_OPEN" }); } catch {};
-    
-    nodes.find(x => x.name === "DeveloperExperimentStore").actionHandler["CONNECTION_OPEN"]({});
-    experimentMod.initialize.call({ waitFor: () => {}, getName: () => {} });
-    
-    gcUserPatch();
+function setExperiments() {
+    userMod.getCurrentUser().flags += 1
+    userMod._dispatcher._actionHandlers._computeOrderedActionHandlers("OVERLAY_INITIALIZE").forEach((function(e) {
+        e.name.includes("Experiment") && e.actionHandler({
+            serializedExperimentStore: findByProps("getSerializedState").getSerializedState(), user: { flags: 1 }
+        })
+    }))
+    userMod.getCurrentUser().flags -= 1
 }
 
-export const onLoad = () => setExperiments(true);
+export const onLoad = () => setExperiments();
 export function onUnload() {
-    setExperiments(false);
     // TODO: Offer to do this for the user
     alert("For developer features to be fully disabled, you must restart Discord.");
 }
