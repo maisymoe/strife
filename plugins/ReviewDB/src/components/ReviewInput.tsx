80f8bb28a6d9875c98597382a0f32fc629c70296
Beef <beefers@riseup.net>
2023-03-12 17:13:54 +0000

    [Plugins > ReviewDB] Improve ReviewInput

--- plugins/ReviewDB/src/components/ReviewInput.tsx
+++ plugins/ReviewDB/src/components/ReviewInput.tsx
@@ -1,13 +1,12 @@
-import { React, ReactNative as RN, stylesheet } from "@vendetta/metro/common";
+import { React, ReactNative as RN, stylesheet, constants } from "@vendetta/metro/common";
+import { findByProps } from "@vendetta/metro";
 import { useProxy } from "@vendetta/storage";
 import { storage } from "@vendetta/plugin";
 import { rawColors } from "@vendetta/ui";
 import { showToast } from "@vendetta/ui/toasts";
 import { getAssetIDByName } from "@vendetta/ui/assets";
-import { Forms } from "@vendetta/ui/components";
 import { addReview } from "../lib/api";
 
-// TODO: This component isn't styled very well.
 // TODO: Properly handle existing reviews - whilst it does work fine right now, add a visual representation
 
 interface ReviewInputProps {
@@ -21,13 +20,13 @@ const styles = stylesheet.createThemedStyleSheet({
         flexDirection: "row",
         alignItems: "center",
         justifyContent: "center",
-        marginBottom: 4,
-        marginHorizontal: 8,
+        marginHorizontal: 4,
     },
     textInput: {
         flex: 1,
-        paddingTop: 0,
-        paddingHorizontal: 0,
+        flexGrow: 1,
+        fontSize: 16,
+        fontFamily: constants.Fonts.DISPLAY_MEDIUM,
     },
     sendButton: {
         flexShrink: 0,
@@ -42,27 +41,33 @@ const styles = stylesheet.createThemedStyleSheet({
     },
 });
 
+const { colors, meta } = findByProps("colors", "meta");
+const useThemeContext = findByProps("useThemeContext").useThemeContext;
+
 export default function ReviewInput({ userId, refetch }: ReviewInputProps) {
     useProxy(storage);
+    const themeContext = useThemeContext();
+    const textColor = meta.resolveSemanticColor(themeContext.theme, colors.TEXT_NORMAL);
+    const placeholderColor = meta.resolveSemanticColor(themeContext.theme, colors.INPUT_PLACEHOLDER_TEXT);
+
     const [reviewText, setReviewText] = React.useState("");
     const disableTextArea = !storage.authToken;
     const disableButton = !storage.authToken || reviewText.length === 0;
 
     return (
         <RN.View style={styles.container}>
-            <Forms.FormInput
-                style={styles.textInput}
+            <RN.TextInput
+                style={{ ...styles.textInput, color: textColor }}
                 editable={!disableTextArea}
                 placeholder={disableTextArea ? "You must be authorised to add a review." : "Tap to add a review"}
-                showBorder={false}
+                placeholderTextColor={placeholderColor}
                 value={reviewText}
-                onChange={(i: string) => setReviewText(i)}
+                onChangeText={(i: string) => setReviewText(i)}
             />
-            <RN.Pressable 
+            <RN.Pressable
                 style={{ ...styles.sendButton, opacity: disableButton ? 0.25 : 1 }}
-                // disabled={disableButton}
+                disabled={disableButton}
                 onPress={() => {
-                    console.log("pressed");
                     addReview(userId, reviewText).then((res) => {
                         setReviewText("");
                         refetch();
