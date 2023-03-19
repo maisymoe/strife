4ecf4fb1928b1d4fdead7b24c4e15800aa281724
Beef <beefers@riseup.net>
2023-03-19 19:37:17 +0000

    [Plugins > PinSettings] "Fix" outstanding issues

--- plugins/PinSettings/src/components/SettingsRow.tsx
+++ plugins/PinSettings/src/components/SettingsRow.tsx
@@ -1,4 +1,4 @@
-import { NavigationNative } from "@vendetta/metro/common";
+import { React, NavigationNative } from "@vendetta/metro/common";
 import { getSettings } from "@vendetta/plugins";
 import { getAssetIDByName } from "@vendetta/ui/assets";
 import { Forms } from "@vendetta/ui/components";
@@ -6,11 +6,12 @@ import { Forms } from "@vendetta/ui/components";
 interface SettingsRowProps {
     // TODO: Oh god, TS is using the old browser Plugin interface here
     plugin: Plugin;
+    forceUpdate: Function,
 }
 
 const { FormRow } = Forms;
 
-export default function SettingsRow({ plugin }: SettingsRowProps) {
+export default function SettingsRow({ plugin, forceUpdate }: SettingsRowProps) {
     const settings = getSettings(plugin.id)
     const navigation = NavigationNative.useNavigation();
     
@@ -19,10 +20,10 @@ export default function SettingsRow({ plugin }: SettingsRowProps) {
             label={plugin.manifest.name}
             leading={<FormRow.Icon source={getAssetIDByName(plugin.manifest.vendetta?.icon ?? "ic_application_command_24px")} />}
             trailing={FormRow.Arrow}
-            onPress={() => navigation.push("VendettaCustomPage", {
+            onPress={() => plugin.enabled ? navigation.push("VendettaCustomPage", {
                 title: plugin.manifest.name,
                 render: settings,
-            })}
+            }) : forceUpdate()}
         />
     )
 }
--- plugins/PinSettings/src/components/SettingsSection.tsx
+++ plugins/PinSettings/src/components/SettingsSection.tsx
@@ -1,3 +1,4 @@
+import { React } from "@vendetta/metro/common";
 import { useProxy } from "@vendetta/storage";
 import { storage } from "@vendetta/plugin";
 import { plugins } from "@vendetta/plugins";
@@ -11,11 +12,13 @@ export default function SettingsSection() {
     const keyedPlugins = Object.keys(storage.pinnedPlugins);
     const hasOtherMods = window.hasOwnProperty("enmity") || window.hasOwnProperty("Aliucord");
 
+    const [, forceUpdate] = React.useReducer((n) => ~n, 0);
+
     if (keyedPlugins.length === 0) return null;
 
-    const mappedRows = keyedPlugins.map((p, i) => (
+    const mappedRows = keyedPlugins.filter(p => plugins[p] && plugins[p].enabled).map((p, i) => (
         <>
-            <SettingsRow plugin={plugins[p]} />
+            <SettingsRow plugin={plugins[p]} forceUpdate={forceUpdate} />
             {i !== keyedPlugins.length - 1 && <FormDivider />}
         </>
     ))
