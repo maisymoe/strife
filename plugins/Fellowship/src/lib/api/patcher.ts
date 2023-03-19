36f121ea5b476d01db862ef7e72552489fe107ee
Beef <beefers@riseup.net>
2023-03-19 01:16:20 +0000

    [Plugins > Fellowship] Typo

--- plugins/Fellowship/src/lib/api/patcher.ts
+++ plugins/Fellowship/src/lib/api/patcher.ts
@@ -13,7 +13,7 @@ function wrapCallback (type: PatchType, callback: Function, original?: Function)
 // Wrappers since Enmity has reversed patcher args order and other quirks
 // TODO: Do I need to actually implement the caller functionality?
 export const before = (caller: string, funcParent: any, funcName: string, callback: Function, oneTime: boolean) => patcher.before(funcName, funcParent, wrapCallback("before", callback, funcParent?.[funcName]), oneTime);
-export const instead = (caller: string, funcParent: any, funcName: string, callback: Function, oneTime: boolean) => patcher.instead(funcName, funcParent, wrapCallback("before", callback), oneTime);
+export const instead = (caller: string, funcParent: any, funcName: string, callback: Function, oneTime: boolean) => patcher.instead(funcName, funcParent, wrapCallback("instead", callback), oneTime);
 export const after = (caller: string, funcParent: any, funcName: string, callback: Function, oneTime: boolean) => patcher.after(funcName, funcParent, wrapCallback("after", callback), oneTime);
 export const unpatchAll = () => stub("unpatchAll");
 
