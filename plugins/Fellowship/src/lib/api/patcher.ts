a19de04e91878a9505c2595cb51844929864dbbf
Beef <beefers@riseup.net>
2023-03-24 21:42:35 +0000

    [Plugins > Fellowship] *sigh* Correctly polyfill patcher

--- plugins/Fellowship/src/lib/api/patcher.ts
+++ plugins/Fellowship/src/lib/api/patcher.ts
@@ -1,8 +1,21 @@
 import { patcher } from "@vendetta";
-import { stub } from "../utils";
 
 type PatchType = "before" | "instead" | "after";
-function wrapCallback (type: PatchType, callback: Function, original?: Function): any {
+type PatchSet = Record<PatchType, Function[]>
+
+const patches: Record<string, PatchSet | undefined> = {};
+
+function pushPatch(caller: string, type: PatchType, patch: () => boolean) {
+    if (!patches[caller]) patches[caller] = {
+        before: [],
+        instead: [],
+        after: [],
+    }
+
+    patches[caller][type].push(patch);
+}
+
+function wrapCallback(type: PatchType, callback: Function, original?: Function): any {
     switch (type) {
         case "before": return (args: any[]) => callback(this, args, original);
         case "instead": return (args: any[], orig: Function) => callback(this, args, orig);
@@ -11,14 +24,20 @@ function wrapCallback (type: PatchType, callback: Function, original?: Function)
 }
 
 // Wrappers since Enmity has reversed patcher args order and other quirks
-// TODO: Do I need to actually implement the caller functionality?
-export const before = (caller: string, funcParent: any, funcName: string, callback: Function, oneTime: boolean) => patcher.before(funcName, funcParent, wrapCallback("before", callback, funcParent?.[funcName]), oneTime);
-export const instead = (caller: string, funcParent: any, funcName: string, callback: Function, oneTime: boolean) => patcher.instead(funcName, funcParent, wrapCallback("instead", callback), oneTime);
-export const after = (caller: string, funcParent: any, funcName: string, callback: Function, oneTime: boolean) => patcher.after(funcName, funcParent, wrapCallback("after", callback), oneTime);
-export const unpatchAll = () => stub("unpatchAll");
+export const before = (caller: string, funcParent: any, funcName: string, callback: Function, oneTime: boolean) =>
+    pushPatch(caller, "before", patcher.before(funcName, funcParent, wrapCallback("before", callback, funcParent?.[funcName]), oneTime));
 
-export const create = () => ({
-    getPatchesByCaller: () => stub("getPatchesByCaller", []),
+export const instead = (caller: string, funcParent: any, funcName: string, callback: Function, oneTime: boolean) =>
+    pushPatch(caller, "instead", patcher.instead(funcName, funcParent, wrapCallback("instead", callback), oneTime));
+
+export const after = (caller: string, funcParent: any, funcName: string, callback: Function, oneTime: boolean) =>
+    pushPatch(caller, "after", patcher.after(funcName, funcParent, wrapCallback("after", callback), oneTime));
+
+export const getPatchesByCaller = (caller: string) => [...patches[caller].before, ...patches[caller].instead, ...patches[caller].after];
+export const unpatchAll = (caller: string) => getPatchesByCaller(caller).forEach(p => p());
+
+export const create = (caller: string) => ({
+    getPatchesByCaller: () => getPatchesByCaller(caller),
     before,
     instead,
     after,
