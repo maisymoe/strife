aa788b7c89035b07a52305161c2c407ef560c45f
Beef <beefers@riseup.net>
2023-03-25 00:17:01 +0000

    [Plugins > Fellowship] Polyfill most of the utils

---
+++ plugins/Fellowship/src/lib/api/utilities.ts
@@ -0,0 +1,17 @@
+import { utils } from "@vendetta";
+import { findByProps } from "@vendetta/metro";
+import { React, ReactNative, lodash } from "@vendetta/metro/common";
+import { stub } from "../utils";
+
+const { uuid4 } = findByProps("uuid4");
+
+export const memoize = (func: (...args: any[]) => any) => lodash.memoize(func);
+export const debounce = (func: ((...args: any[]) => any), ms: number) => lodash.debounce(func, ms);
+export const createStore = stub("createStore", {});
+export const findInReactTree = (tree: object, filter = (i: any) => i, options = {}) => utils.findInTree(tree, filter, { walkable: ["props", "children"], ...options });
+export const normalizeSize = ReactNative.PixelRatio.getPixelSizeForLayoutSize;
+export const wrapInHooks = stub("wrapInHooks", React.createElement(ReactNative.Text, {}, "Stubbed wrapInHooks"));
+export const findInTree = (tree = {}, filter = (i: any) => i, { ignore = [], walkable = [], maxProperties = 100 } = {}) => utils.findInTree(tree, filter, { ignore, walkable, maxDepth: maxProperties });
+export const sleep = (time: number) => new Promise(i => setTimeout(i, time));
+// TODO: Enmity's supports a length argument. Do we need to?
+export const uuid = uuid4;
--- plugins/Fellowship/src/lib/windowObject.ts
+++ plugins/Fellowship/src/lib/windowObject.ts
@@ -1,15 +1,19 @@
+import { plugin } from "@vendetta";
+import { General, Forms } from "@vendetta/ui/components";
 import * as metro from "./api/metro";
 import * as common from "./api/metro/common";
 import * as patcher from "./api/patcher";
+import * as utilities from "./api/utilities";
 import * as native from "./api/native";
 import * as assets from "./api/assets";
-import { plugin } from "@vendetta";
 
 export default () => {
     globalThis.enmity = {
         modules: { ...metro, common: common },
         patcher,
         version: `Fellowship ${plugin.manifest.hash.slice(7)}`,
+        utilities,
+        components: { ...General, ...Forms },
         native,
         assets,
     };
